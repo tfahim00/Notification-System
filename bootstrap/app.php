8f9f65a55da0e09a3a844bed21714082366d9fe3
fahim <tfahim00@gmail.com>
2025-10-06 19:02:19 +0600

    Feat: Added Automatic Send Notification Functionality

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('notifications:send')->dailyAt('09:00');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
